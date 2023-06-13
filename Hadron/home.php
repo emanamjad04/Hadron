887ac7d73c972d169cc19bf3e4754ad1cf1c7a2c
justmexmehak <119690712+justmexmehak@users.noreply.github.com>
2023-06-13 19:25:54 +0500

    Update home.php

--- Hadron/home.php
+++ Hadron/home.php
@@ -2,6 +2,8 @@
 <html lang="en">
 <head>
 <link rel="stylesheet" type="text/css" href="allcss.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
 
     <?php
     
